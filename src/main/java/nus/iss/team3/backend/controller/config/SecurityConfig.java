98a7a96896afa7bcafc8a2d302f48e015ae1d81f
Desmond Tan <desmondtzh@hotmail.com>
2024-11-14 11:57:08 +0800

    feat: enable the jwt token and cors.allowed.origins to be read from .env

--- src/main/java/nus/iss/team3/backend/controller/config/SecurityConfig.java
+++ src/main/java/nus/iss/team3/backend/controller/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package nus.iss.team3.backend.controller.config;
 
 import java.util.List;
 import nus.iss.team3.backend.service.jwt.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @Value("#{'${cors.allowed.origins}'.split(',')}")
+  private List<String> allowedOrigins;
+
   private final JwtRequestFilter jwtRequestFilter;
 
   public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
@@ -40,8 +44,6 @@ public class SecurityConfig {
                     .authenticated()
                     .requestMatchers("/recipe/**")
                     .authenticated()
-                    .requestMatchers("/recipe/**")
-                    .authenticated()
                     .requestMatchers("/user/**")
                     .authenticated()
                     .anyRequest()
@@ -54,10 +56,16 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedOrigins(allowedOrigins);
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(
-        List.of("Authorization", "Cache-Control", "Content-Type", "X-XSRF-TOKEN", "userId"));
+        List.of(
+            "Authorization",
+            "Cache-Control",
+            "Content-Type",
+            "X-XSRF-TOKEN",
+            "userId",
+            "Internal-Service-Call"));
     configuration.setAllowCredentials(true);
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
--- src/main/java/nus/iss/team3/backend/service/jwt/JwtUtil.java
+++ src/main/java/nus/iss/team3/backend/service/jwt/JwtUtil.java
@@ -3,6 +3,7 @@ package nus.iss.team3.backend.service.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
@@ -11,25 +12,22 @@ import java.util.Map;
 import java.util.function.Function;
 import javax.crypto.SecretKey;
 import nus.iss.team3.backend.entity.UserAccount;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUtil {
-  private String SECRET_KEY =
-      Base64.getEncoder()
-          .encodeToString(
-              "Th1sIsN0tThePassw0rdD0ntA$kMeThePasswordIAls0DontknowtheactualPassword".getBytes());
 
-  // 4261656C64756E67
-  // This1sNotThePassW0rd
+  @Value(
+      "${Just.Wonder.This:Th1sIsN0tThePassw0rdD0ntA$kMeThePasswordIAls0DontknowtheactualPassword}")
+  private String SECRET_DOOR_OPENER;
 
-  //  feats-backend-authenticate  | io.jsonwebtoken.security.WeakKeyException: The signing key's
-  // size is 120 bits which is not secure enough for the HS256 algorithm.  The JWT JWA Specification
-  // (RFC 7518, Section 3.2) states that keys used with HS256 MUST have a size >= 256 bits (the key
-  // size must be greater than or equal to the hash output size).  Consider using the
-  // io.jsonwebtoken.security.Keys class's 'secretKeyFor(SignatureAlgorithm.HS256)' method to create
-  // a key guaranteed to be secure enough for HS256.  See
-  // https://tools.ietf.org/html/rfc7518#section-3.2 for more information.
+  private String SECRET_KEY = null;
+
+  @PostConstruct
+  public void postConstruct() {
+    SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_DOOR_OPENER.getBytes());
+  }
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
